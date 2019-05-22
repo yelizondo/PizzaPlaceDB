1cb7fd18c1f306262234f8051269265abee95b1e
yelizondo <yubeg971@gmail.com>
2019-05-21 20:09:27 -0600

    Nuevo archivos de mantenimiento annadidos

--- Pizzeria/routes/Mantenimiento.js
+++ Pizzeria/routes/Mantenimiento.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('Mantenimiento', {
+      title: 'Mantenimiento',
+      style: 'Mantenimiento.css',
+      resIngredientes: ['Apio','Ajo']
+  });
 });
 
 module.exports = router;
